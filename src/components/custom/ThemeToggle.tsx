40819e6a561e41838aa7625629cdebd82d296f50
Yogesh Shahi <yogeshshahi120@gmail.com>
2025-06-25 18:36:40 +0530

    feat: added nav bar for desktop version

---
+++ src/components/custom/ThemeToggle.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { MoonIcon, SunDimIcon } from '@phosphor-icons/react';
+import { motion } from 'motion/react';
+import React, { useCallback } from 'react';
+
+import { Button } from '../ui/button';
+
+const ThemeToggle = ({ theme, setTheme, mounted }: any) => {
+  const handleLightTheme = useCallback(() => setTheme('light'), [setTheme]);
+  const handleDarkTheme = useCallback(() => setTheme('dark'), [setTheme]);
+
+  if (!mounted) return null;
+
+  return (
+    <div className='border-input relative flex gap-2 rounded-xl border p-1'>
+      <motion.div
+        className='bg-background border-input absolute size-8 rounded-lg border shadow-sm'
+        layout
+        initial={false}
+        animate={{ x: theme === 'dark' ? 40 : 0 }}
+        transition={{
+          type: 'spring',
+          visualDuration: 0.25,
+          bounce: 0.3,
+        }}
+      />
+      <Button
+        variant='ghost'
+        size='smIcon'
+        onClick={handleLightTheme}
+        aria-label='Switch to light theme'
+        aria-pressed={theme === 'light'}
+        className='transition-theme relative z-10 rounded-lg'
+      >
+        <SunDimIcon
+          size={24}
+          color='#3B3B3B'
+          weight={theme === 'light' ? 'fill' : 'regular'}
+          className='transition-all'
+        />
+      </Button>
+      <Button
+        variant='ghost'
+        size='smIcon'
+        onClick={handleDarkTheme}
+        aria-label='Switch to dark theme'
+        aria-pressed={theme === 'dark'}
+        className='transition-theme relative z-10 rounded-lg'
+      >
+        <MoonIcon
+          size={24}
+          color='#3B3B3B'
+          weight={theme === 'dark' ? 'fill' : 'regular'}
+          className='transition-all'
+        />
+      </Button>
+    </div>
+  );
+};
+
+export default ThemeToggle;
