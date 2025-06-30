6d882a5127acfe6e4b34911981335963c61f2935
Yogesh Shahi <yogeshshahi120@gmail.com>
2025-06-30 19:03:36 +0530

    feat: added contribute page and changed content for the same

--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -6,21 +6,21 @@ import EmailUploader from './EmailUploader';
 export default function Home() {
   return (
     <div className='my-8 flex flex-col items-center justify-center'>
-      <div className='border-border relative mx-auto aspect-12/5 w-14/15 max-w-[720px] overflow-clip rounded-t-3xl border md:aspect-9/2'>
+      <div className='border-border relative mx-auto aspect-12/5 w-14/15 max-w-[720px] overflow-clip rounded-t-3xl border'>
         <div className='absolute bottom-0 z-40 inline-flex flex-col items-start justify-start p-6'>
           <div className='flex justify-start text-[clamp(2rem,3.34vw,3rem)] font-bold text-white capitalize'>
-            DKIM Archive
+            Contribute
           </div>
           <div className='flex justify-start text-[clamp(1rem,1.39vw,1.25rem)] leading-5 font-semibold tracking-tight text-white'>
             <span className='whitespace-normal'>
-              Building the largest open-sourced directory of DKIM pairs.&nbsp;
-              <span className='underline'>Read More</span>
+              By uploading DKIM pairs from your Gmail account or from a TSV
+              file.
             </span>
           </div>
         </div>
         <div className='absolute -bottom-1 z-20 h-40 w-full bg-gradient-to-b from-sky-900/0 to-sky-900/90 blur-[2px] md:h-20'></div>
         <Image
-          src='/header-home.png'
+          src='/header-contribute.png'
           alt='Home page header image'
           fill={true}
           className='object-cover'
