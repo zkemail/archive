cff4eff90fdce5957c3f83918f210aee45342227
Yogesh Shahi <yogeshshahi120@gmail.com>
2025-12-22 19:07:34 +0530

    feat: integrated SessionProvider in root layout
    - wrapped app with SessionProvider for auth state
    - removed unused GoogleOAuthProvider import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css';
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import type { Metadata } from 'next';
 import { Fustat } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
 
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import { SessionProvider } from '@/components/SessionProvider';
 import ToastProvider from '@/contexts/ToastProvider';
 
 const fustat = Fustat({ subsets: ['latin'] });
@@ -36,13 +36,15 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={`${fustat.className} transition-theme antialiased`}>
-        <ThemeProvider attribute='class' enableSystem defaultTheme='system'>
-          <div className='transition-theme flex h-screen flex-col justify-between'>
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </ThemeProvider>
+        <SessionProvider>
+          <ThemeProvider attribute='class' enableSystem defaultTheme='system'>
+            <div className='transition-theme flex h-screen flex-col justify-between'>
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
